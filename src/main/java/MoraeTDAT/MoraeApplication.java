f73ad6e14099d15aa7b9ab0482db509afc9db00d
KSJ <sjkim@businessinsight.co.kr>
2023-08-24 13:03:36 +0900

    [김상재] 2

---
+++ src/main/java/MoraeTDAT/MoraeApplication.java
@@ -0,0 +1,13 @@
+package MoraeTDAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoraeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoraeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MoraeTDAT/MoraeApplicationTests.java
@@ -0,0 +1,13 @@
+package MoraeTDAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoraeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
