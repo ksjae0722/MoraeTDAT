f3864b6580478a98fd4a81c0596acf119e42701b
KSJ <sjkim@businessinsight.co.kr>
2023-08-30 16:55:49 +0900

    [김상재] 드디어 화면 나옴 도라방방방방방스

--- src/main/java/MoraeTdat/Controller/TestController.java
+++ src/main/java/MoraeTdat/Controller/TestController.java
@@ -3,16 +3,18 @@ package MoraeTdat.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 
 public class TestController {
-	@RequestMapping(value="/main")
-	public ModelAndView hello(Model model) {
-		ModelAndView mav = new ModelAndView();
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public ModelAndView hello(ModelAndView mav) {
 		
 		mav.setViewName("/main");
+		mav.addObject("ID", "admin");
 				
 		return mav;
 	}
